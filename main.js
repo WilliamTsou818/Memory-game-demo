8428f6b0653c120e43893c67e5cb4eea1b9865ea
WilliamTsou818 <cccc1589@gmail.com>
2021-05-11 18:44:50 +0800

    Complete the memory game demo

---
+++ main.js
@@ -0,0 +1,171 @@
+// game state
+const GAME_STATE = {
+  FirstCardAwaits: 'FirstCardAwaits',
+  SecondCardAwaits: 'SecondCardAwaits',
+  CardsMatchFailed: 'CardsMatchFailed',
+  CardsMatched: 'CardsMatched',
+  GameFinished: 'GameFinished'
+}
+
+
+// card data
+const Symbols = [
+  'https://image.flaticon.com/icons/svg/105/105223.svg', // 黑桃
+  'https://image.flaticon.com/icons/svg/105/105220.svg', // 愛心
+  'https://image.flaticon.com/icons/svg/105/105212.svg', // 方塊
+  'https://image.flaticon.com/icons/svg/105/105219.svg' // 梅花
+]
+
+// view part
+const view = {
+  getCardContent(index) {
+    const number = this.transformNumber((index % 13) + 1)
+    const symbol = Symbols[Math.floor(index / 13)]
+    return `<p>${number}</p>
+      <img src="${symbol}">
+      <p>${number}</p>`
+  },
+
+  getCardElement(index) {
+    return `<div class="card back" data-index="${index}"></div>`
+  },
+
+  transformNumber(number) {
+    switch (number) {
+      case 1:
+        return 'A'
+      case 11:
+        return 'J'
+      case 12:
+        return 'Q'
+      case 13:
+        return 'K'
+      default:
+        return number
+    }
+  },
+
+  flipCards(...cards) {
+    cards.map(card => {
+      if (card.classList.contains('back')) {
+        card.classList.remove('back')
+        card.innerHTML = this.getCardContent(Number(card.dataset.index))
+        return
+      }
+      card.classList.add('back')
+      card.innerHTML = null
+    })
+  },
+
+  displayCards(indexes) {
+    const rootElement = document.querySelector('#cards')
+    rootElement.innerHTML = indexes.map(index => this.getCardElement(index))
+      .join("")
+  },
+
+  pairCards(...cards) {
+    cards.map(card => {
+      card.classList.add('paired')
+    })
+  },
+
+  renderScore(score) {
+    document.querySelector('.score').textContent = `Score: ${score}`
+  },
+
+  renderTriedTimes(times) {
+    document.querySelector('.tried').textContent = `You've tried: ${times} times`
+  },
+
+  appendWrongAnimation(...cards) {
+    cards.map(card => {
+      card.classList.add('wrong')
+      card.addEventListener('animationend', event =>
+        event.target.classList.remove('wrong'), { once: true })
+    })
+  }
+}
+
+// extension tools
+const utility = {
+  getRandomNumberArray(count) {
+    const number = Array.from(Array(count).keys())
+    for (let index = number.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1));
+      [number[index], number[randomIndex]] = [number[randomIndex], number[index]]
+    }
+    return number
+  }
+}
+
+// controller part
+const controller = {
+  currentState: GAME_STATE.FirstCardAwaits,
+
+  generateCards() {
+    view.displayCards(utility.getRandomNumberArray(52))
+  },
+
+  dispatchCardAction(card) {
+    if (!card.classList.contains('back')) {
+      return
+    }
+
+    switch (this.currentState) {
+      case GAME_STATE.FirstCardAwaits:
+        view.flipCards(card)
+        model.revealedCards.push(card)
+        this.currentState = GAME_STATE.SecondCardAwaits
+        break
+      case GAME_STATE.SecondCardAwaits:
+        view.renderTriedTimes(++model.triedTimes)
+        view.flipCards(card)
+        model.revealedCards.push(card)
+
+        // check realed cards match or not
+        if (model.isRevealedCardsMatched()) {
+          // matched
+          view.renderScore(model.score += 10)
+          this.currentState = GAME_STATE.CardsMatched
+          view.pairCards(...model.revealedCards)
+          model.revealedCards = []
+          this.currentState = GAME_STATE.FirstCardAwaits
+        } else {
+          // failed
+          this.currentState = GAME_STATE.CardsMatchFailed
+          view.appendWrongAnimation(...model.revealedCards)
+          setTimeout(this.resetCards, 1000)
+        }
+        break
+    }
+  },
+
+  resetCards() {
+    view.flipCards(...model.revealedCards)
+    model.revealedCards = []
+    controller.currentState = GAME_STATE.FirstCardAwaits
+  }
+}
+
+// model part
+const model = {
+  revealedCards: [],
+  score: 0,
+  triedTimes: 0,
+
+  isRevealedCardsMatched() {
+    return this.revealedCards[0].dataset.index % 13 === this.revealedCards[1].dataset.index % 13
+  }
+}
+
+
+
+// execute
+controller.generateCards()
+
+// add event listener to each card
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('click', event => {
+    controller.dispatchCardAction(card)
+  })
+})
